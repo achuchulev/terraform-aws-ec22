ed285c1252ae8ca207f7f3d65f6702f0bd6f9aea
Atanas Chuchulev <atanas.v4@gmail.com>
2020-12-08 19:00:57 +0200

    Update and rename modules/ec2/variables.tf to variables.tf

--- variables.tf
+++ variables.tf
@@ -11,23 +11,27 @@ variable "region" {
   description = "AWS region"
 }
 
+variable "aws_token" {
+}
+
 variable "ami" {
   description = "AWS AMI id"
 }
 
 variable "instance_type" {
   description = "AWS instance type"
+  default     = "t2.micro"
 }
 
 variable "subnet_id" {
   description = "AWS subnet id"
 }
 
-variable "public_key" {
-  description = "A public key for resource aws_key_pair that generates a key pair on AWS for the instance"
+variable "aws_key_pair_name" {
+  description = "An existing aws key pair name for the instance"
 }
 
 variable "vpc_security_group_ids" {
-  type        = "list"
+  type        = list(string)
   description = "AWS VPC security group ids"
 }
